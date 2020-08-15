1a9a39aaef81fca320173653b74d00525731a47f
vorpal56 <anthonysukadil1@gmail.com>
2020-08-15 13:34:56 -0400

    separated champion and level selection into champion component, removed target dummy as a selectable target as it's not realistic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, Input, ViewChild, ChangeDetectionStrategy, ChangeDetectorRef } from "@angular/core";
-import { CHAMPIONS, LEVELS } from "./data";
+import { Component, OnInit, ViewChild, ChangeDetectionStrategy, ChangeDetectorRef } from "@angular/core";
 import { Champion } from "./models/champion";
 import { Item } from "./models/item";
 import { ChampionService } from './services/champion.service';
@@ -13,19 +12,20 @@ import { EMPTY_ITEM } from './items';
 })
 export class AppComponent implements OnInit {
 	title = "league-theory-crafter";
-	champions = CHAMPIONS;
-	levels = LEVELS;
 
-	champion: Champion = this.champions[2];
+	// item definition from the item-selector component to the inventory component
 	selectedItems: [Item, Item, Item, Item, Item, Item] = [EMPTY_ITEM, EMPTY_ITEM, EMPTY_ITEM, EMPTY_ITEM, EMPTY_ITEM, EMPTY_ITEM];
 	selectedElixir: Item = EMPTY_ITEM;
 	selectedItemRestrictions = { "hasGoldOrJg": false, "hasBoots": false, "hasTear": false, "hasSealOrMejais": false, "masterworkItems": [EMPTY_ITEM, EMPTY_ITEM], "hasHexcore": false };
 	numberOfEquippedItems = 0;
 
+	// rune/item-selector definition to show which page first
 	selectedTab: string = "runes";
 
-	currentLevel: number = LEVELS[9].levelValue;
-	currentLevelName: string = LEVELS[0].levelLabel;
+	// champion and current level definition from the champion component
+	champion: Champion;
+	currentLevel: number;
+
 	@ViewChild(InventoryComponent) inventoryComponent: InventoryComponent;
 	/**
 	 * Method that updates the champion details
@@ -33,12 +33,26 @@ export class AppComponent implements OnInit {
 	 * @returns void
 	 */
 	updateChampion(): void {
-		// this.levelSelectorEnabled = true; 
-		this.inventoryComponent.emitSelectedItems();
-		this.inventoryComponent.removeInvalidItemsBasedOnChampion(this.champion, this.currentLevel);
-		this.inventoryComponent.removeInvalidElixirBasedOnLevel(this.currentLevel);
+		if (this.inventoryComponent) {
+			this.inventoryComponent.emitSelectedItems();
+			this.inventoryComponent.removeInvalidItemsBasedOnChampion(this.champion, this.currentLevel);
+			this.inventoryComponent.removeInvalidElixirBasedOnLevel(this.currentLevel);
+		}
 		this.championService.adjustBaseAndItemStats(this.champion, this.currentLevel, this.selectedItems, this.selectedElixir);
 		return;
+	}
+	setChampion(selectedChampion: Champion) {
+		if (selectedChampion != this.champion) {
+			this.champion = selectedChampion;
+			this.updateChampion();
+		}
+		return;
+	}
+	setCurrentLevel(selectedLevel: number) {
+		if (selectedLevel != this.currentLevel) {
+			this.currentLevel = selectedLevel;
+			this.updateChampion();
+		}
 	}
 	setSelectedItemRestrictions(selectedItemRestrictions: any) {
 		this.selectedItemRestrictions = selectedItemRestrictions;
@@ -64,6 +78,6 @@ export class AppComponent implements OnInit {
 		this.cdRef.detectChanges();
 	}
 	ngOnInit() {
-		this.championService.adjustBaseAndItemStats(this.champion, this.currentLevel, this.selectedItems, this.selectedElixir);
+		// this.championService.adjustBaseAndItemStats(this.champion, this.currentLevel, this.selectedItems, this.selectedElixir);
 	}
 }
--- src/app/champion/champion.component.ts
+++ src/app/champion/champion.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 import { Champion } from "../models/champion";
 import { ChampionService } from '../services/champion.service';
+import { CHAMPIONS, LEVELS } from '../data';
 
 @Component({
 	selector: "champion",
@@ -9,7 +10,25 @@ import { ChampionService } from '../services/champion.service';
 })
 export class ChampionComponent implements OnInit {
 	constructor(private championService: ChampionService) { }
-	@Input() champion: Champion;
 
-	ngOnInit(): void { }
+	champions = CHAMPIONS;
+	levels = LEVELS;
+
+	@Output('selectedChampion') selectedChampionEventEmitter = new EventEmitter<Champion>();
+	@Output('selectedLevel') currentLevelEventEmitter = new EventEmitter<number>();
+
+	champion: Champion = this.champions[1];
+	currentLevel: number = LEVELS[9].levelValue;
+
+
+	ngOnInit(): void {
+		this.updateChampion();
+		this.updateLevel();
+	}
+	updateChampion() {
+		this.selectedChampionEventEmitter.emit(this.champion);
+	}
+	updateLevel() {
+		this.currentLevelEventEmitter.emit(this.currentLevel);
+	}
 }
