9876a5e9f0874ebb38c1c8c37863d1ae6d41ef7e
vorpal56 <anthonysukadil1@gmail.com>
2020-08-19 19:51:33 -0400

    updated the design and impleted champion service logic to champion component

--- src/app/champion/champion.component.ts
+++ src/app/champion/champion.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { Component, OnInit, Output, EventEmitter, Input } from "@angular/core";
 import { Champion } from "../models/champion";
-import { CHAMPIONS, LEVELS } from '../data';
+import { CHAMPIONS } from '../../data/champions';
+import { LEVELS, TIMES } from '../../data/data';
+import { ChampionService } from '../services/champion.service';
+import { Item } from '../models/item';
 
 @Component({
 	selector: "champion",
@@ -8,32 +11,48 @@ import { CHAMPIONS, LEVELS } from '../data';
 	styleUrls: ["./champion.component.css"],
 })
 export class ChampionComponent implements OnInit {
-	constructor() { }
-
 	champions = CHAMPIONS;
 	levels = LEVELS;
+	times = TIMES;
+
+	@Input("selectedItems") selectedItems: [Item, Item, Item, Item, Item, Item];
+	@Input("selectedElixir") selectedElixir: Item;
+	@Input("selectedRunes") selectedRunes: any;
+	@Input("stackAllRunes") stackAllRunes: boolean;
 
 	@Output('selectedChampion') selectedChampionEventEmitter = new EventEmitter<Champion>();
 	@Output('selectedLevel') currentLevelEventEmitter = new EventEmitter<number>();
+	@Output('selectedTime') currentTimeEventEmitter = new EventEmitter<number>();
 
 	champion: Champion = this.champions[8];
-	currentLevel: number = LEVELS[9].levelValue;
+	currentLevel: number = this.levels[4].levelValue;
+	currentTime: number = this.times[0].timeValue;
+
+	constructor(private championService: ChampionService) { }
 
 	ngOnInit(): void {
-		this.updateChampion();
-		this.updateLevel();
-	}
+		this.selectedChampionEventEmitter.emit(this.champion);
+		this.currentLevelEventEmitter.emit(this.currentLevel);
+		this.currentTimeEventEmitter.emit(this.currentTime);
+		return;
+	};
 	updateChampion() {
 		this.selectedChampionEventEmitter.emit(this.champion);
+		this.championService.applyAllComponentChanges(this.champion, this.currentLevel, this.currentTime, this.selectedItems, this.selectedElixir, this.selectedRunes, this.stackAllRunes);
+		return;
 	}
 	updateLevel() {
 		this.currentLevelEventEmitter.emit(this.currentLevel);
+		this.championService.applyAllComponentChanges(this.champion, this.currentLevel, this.currentTime, this.selectedItems, this.selectedElixir, this.selectedRunes, this.stackAllRunes);
+		return;
 	}
-
+	updateCurrentTime() {
+		this.currentTimeEventEmitter.emit(this.currentTime);
+		this.championService.applyAllComponentChanges(this.champion, this.currentLevel, this.currentTime, this.selectedItems, this.selectedElixir, this.selectedRunes, this.stackAllRunes);
+		return;
+	}
 	isValidStatName(statName: string) {
-		// console.log(statName);
 		if (statName == "ad" || statName == "ap" || statName == "ls" || statName == "spell_vamp" || statName == "arm" || statName == "mr" || statName == "apen" || statName == "mpen" || statName == "as" || statName == "cdr" || statName == "range" || statName == "critdmg" || statName == "crit" || statName == "ms" || statName == "tenacity" || statName == "heal_shield") {
-			console.log(statName);
 			return true;
 		}
 		return false;
