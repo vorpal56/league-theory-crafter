341239f742ce80c2c6a4df1e91a1c18a38beb847
vorpal56 <anthonysukadil1@gmail.com>
2020-08-31 01:17:08 -0400

    updated footer, moved some icon assets

--- src/app/tooltip-options.ts
+++ src/app/tooltip-options.ts
@@ -3,7 +3,7 @@ import { TooltipOptions } from 'ng2-tooltip-directive';
 export const DefaultTooltipOptions: TooltipOptions = {
 	"content-type": "html",
 	"placement": "bottom",
-	"tooltip-class": "tooltips",
+	// "tooltip-class": "tooltips",
 	"hide-delay": 0,
 	"max-width": 300,
 };
